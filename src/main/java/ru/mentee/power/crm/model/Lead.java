fad6472da0dea26dbe8879451eb381b5d0e4e142
Aida Glubokova <glubokova_aida@mail.ru>
2026-03-02 18:10:24 +0500

    BCORE-8: Map для быстрого поиска

---
+++ src/main/java/ru/mentee/power/crm/model/Lead.java
@@ -0,0 +1,12 @@
+package ru.mentee.power.crm.model;
+
+import java.util.UUID;
+
+public record Lead(
+        UUID id,
+        String email,
+        String phone,
+        String company,
+        String status
+) {
+}
